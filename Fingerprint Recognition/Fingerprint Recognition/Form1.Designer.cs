1dac6c4afe5a52c23b482ef4797b3d201847e24e
hsieh-ming <allenhsieh3517@gmail.com>
2024-10-28 14:32:45 +0800

    Creat C# Fingerprint Recognition

--- Fingerprint Recognition/Fingerprint Recognition/Form1.Designer.cs
+++ Fingerprint Recognition/Fingerprint Recognition/Form1.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pictureBox1 = new PictureBox();
+            groupBox1 = new GroupBox();
+            pictureBox2 = new PictureBox();
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(192, 22);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(180, 248);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(pictureBox2);
+            groupBox1.Controls.Add(pictureBox1);
+            groupBox1.Location = new Point(531, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(380, 279);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "檢測結果";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(6, 22);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(180, 248);
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(64, 17);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(109, 23);
+            comboBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 15);
+            label1.TabIndex = 3;
+            label1.Text = "序列埠:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(923, 618);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private GroupBox groupBox1;
+        private PictureBox pictureBox2;
+        private ComboBox comboBox1;
+        private Label label1;
     }
 }
